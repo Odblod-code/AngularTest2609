cb31e2186141956fd596061248e623cb71a7c78d
xpagad34 <xpagad34@ad.aareon.com>
2022-09-26 10:30:55 +0200

    step 2

--- src/app/simple-form.component.ts
+++ src/app/simple-form.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Title } from "./title.model";
+import { TitleService } from "./title.service";
 
 @Component({
-  selector: 'simple-form',
-  templateUrl: './simple-form.component.html',
-  styleUrls: ['./simple-form.component.css']
+  selector: "simple-form",
+  templateUrl: "./simple-form.component.html",
+  styleUrls: ["./simple-form.component.css"],
 })
-export class SimpleFormComponent {}
+export class SimpleFormComponent {
+
+  titleList: Title[];
+  testForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private titleService: TitleService) {
+    this.titleService.getTitles().subscribe(titles => {
+      this.titleList = titles;
+    });
+    this.createForm();
+  }
+   createForm() {
+    this.testForm = this.fb.group({
+       name: ['', Validators.required ]
+    });
+  }
+}
